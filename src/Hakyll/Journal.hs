267ab4bca536cb31af9c32a762d62f8291863d1a
Travis Cardwell <travis.cardwell@extellisys.com>
2014-11-05 20:45:57 +0900

    add project stub

---
+++ src/Hakyll/Journal.hs
@@ -0,0 +1,16 @@
+------------------------------------------------------------------------------
+-- |
+--  Module      : Hakyll.Journal
+--  Description : a Hakyll module journals/diaries
+--  Copyright   : (c) 2014, Travis Cardwell and Rob Nugen
+--  License     : MIT
+--  Maintainer  : Travis Cardwell <travis.cardwell@extellisys.com>
+--  Stability   : experimental
+--
+-- Hakyll Journal is a Hakyll module for adding a journal/diary to your static
+-- website.
+------------------------------------------------------------------------------
+
+module Hakyll.Journal
+  (
+  ) where
